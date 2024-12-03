ef5607b66ef5d8153597c9bb18d22f35f3e43368
Danique Wijk <d.van.der.wijk.2@student.rug.nl>
2024-12-03 15:03:10 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#
+# File name: main.py
+# Author: Danique van der Wijk
+# Student number: s3989771
+# Last updated: 3 December 2024
+# Description: The main file of the thesis
+#
+
+
+def main():
+    """ """
+
+
+
+if __name__ == "__main__":
+    main()
